fcca2e088045c6593270197adbd5929c75032d61
Ankur Kuchlous <akuchlous@gmail.com>
2015-07-04 19:20:05 -0700

    solution 1,2

---
+++ 1/1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+if 64 - 64: i11iIiiIii
+if 65 - 65: O0 / iIii1I11I1II1 % OoooooooOO - i1IIi
+for o0OO00 in range ( 1 , 1000 ) :
+ oo = 0
+ for i1iII1IiiIiI1 in range ( 1 , 1000 ) :
+  if ( i1iII1IiiIiI1 % 3 == 0 or i1iII1IiiIiI1 % 5 == 0 ) :
+   oo += i1iII1IiiIiI1
+   if 40 - 40: ooOoO0O00 * IIiIiII11i
+print oo
+# dd678faae9ac167bc83abf78e5cb2f3f0688d3a3
---
+++ 2/2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+if 64 - 64: i11iIiiIii
+OO0o = 1
+Oo0Ooo = 2
+O0O0OO0O0O0 = 0
+iiiii = 4000000
+ooo0OO = Oo0Ooo
+while ( ooo0OO < iiiii ) :
+ if ( ooo0OO % 2 == 0 ) :
+  O0O0OO0O0O0 += ooo0OO
+  print ooo0OO , O0O0OO0O0O0
+ ooo0OO = OO0o + Oo0Ooo
+ OO0o = Oo0Ooo
+ Oo0Ooo = ooo0OO
+ if 18 - 18: II111iiii . OOO0O / II1Ii / oo * OoO0O00
+# dd678faae9ac167bc83abf78e5cb2f3f0688d3a3
