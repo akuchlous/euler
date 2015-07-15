ae6936dbdc87ff252f654a394c858f5f5bc7e14b
Ankur Kuchlous <akuchlous@gmail.com>
2015-07-15 10:43:48 -0700

    28

---
+++ 28/28.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+if 64 - 64: i11iIiiIii
+def OO0o ( num ) :
+ sum = 1
+ for Oo0Ooo in range ( 2 , num + 1 ) :
+  if ( Oo0Ooo % 2 != 0 ) :
+   sum += Oo0Ooo * Oo0Ooo + Oo0Ooo * Oo0Ooo - ( 1 * ( Oo0Ooo - 1 ) ) + Oo0Ooo * Oo0Ooo - ( 2 * ( Oo0Ooo - 1 ) ) + Oo0Ooo * Oo0Ooo - ( 3 * ( Oo0Ooo - 1 ) )
+ print sum
+OO0o ( 1001 )
+if 85 - 85: OOO0O0O0ooooo % IIii1I . II1 - O00ooooo00
+if 32 - 32: ooOoO + iIiiiI1IiI1I1 * IIiIiII11i * o0oOOo0O0Ooo
+# dd678faae9ac167bc83abf78e5cb2f3f0688d3a3
